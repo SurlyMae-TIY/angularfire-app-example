2bf47c5d7d5b6fda29b734c6d5bf0810ec684d7b
SurlyMae <surlymae@gmail.com>
2016-11-23 14:18:33 -0700

    chore(init): First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module("todoApp", ['firebase']);
+
+app.controller("TodoCtrl", function($scope, $firebaseArray, $firebaseObject) {
+  var ref = firebase.database().ref().child('todos');
+  this.todos = $firebaseArray(ref);
+  // [
+  //   "reference https://github.com/firebase/angularfire",
+  //   "reference https://github.com/firebase/angularfire/blob/master/docs/quickstart.md",
+  //   "reference https://github.com/firebase/angularfire/blob/master/docs/reference.md",
+  //   "Give Sean high quality feedback on the classroom section of bootcamp",
+  //   "Have a great Thanksgiving holiday",
+  //   "Have more confidence in your coding capabilities!",
+  //   "Build an awesome personal project"
+  // ]
+  this.addNewTodo = function(keyEvent) {
+    if (keyEvent === undefined || keyEvent.which === 13) {
+      // this.todos.push(this.newTodo)
+      this.todos.$add(this.newTodo)
+      this.newTodo = ''
+    }
+  }
+  this.triggerEditMode = function() {
+    this.editMode = !this.editMode
+  }
+  this.editTask = function(todo) {
+    this.todos.$save(todo)
+  }
+  this.deleteTodo = function(todo) {
+    // this.todos.splice(index, 1);
+    this.todos.$remove(todo)
+  }
+})
